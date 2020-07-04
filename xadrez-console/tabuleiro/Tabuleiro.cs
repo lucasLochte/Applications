1b993835afd27d24a0c3f05d08cb49670acd7ad1
Lucas Henrique <lucas.lochte@gmail.com>
2020-07-04 20:21:19 -0300

    Comecando a implementar o tabuleiro

---
+++ xadrez-console/tabuleiro/Tabuleiro.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace xadrez_console.tabuleiro
+{
+    class Tabuleiro
+    {
+        public int Linhas { get; set; }
+        public int Colunas { get; set; }
+        public Peca[,] Pecas;
+        public Tabuleiro(int linhas, int colunas)
+        {
+            Linhas = linhas;
+            Colunas = colunas;
+            Pecas = new Peca[linhas, colunas];
+        }
+    }
+}

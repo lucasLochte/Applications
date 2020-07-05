6091efadfbb405feac32ab4c132ea936c2ce7fb5
Lucas Henrique <lucas.lochte@gmail.com>
2020-07-04 22:01:36 -0300

    Tratando erros com excecao personalizada

---
+++ xadrez-console/tabuleiro/TabuleiroException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace xadrez_console.tabuleiro
+{
+    class TabuleiroException : ApplicationException
+    {
+        public TabuleiroException(string message) : base(message)
+        {
+        }
+    }
+}
